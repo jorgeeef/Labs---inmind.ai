40508cefd267b15ce6db9436b9bb3a75b4bbde3c
Georges FARDISSI <fardissi.georges@gmail.com>
2025-02-16 22:56:11 +0200

    Modifying SQL queries

--- Lab03/obj/Debug/net8.0/Lab03.AssemblyInfo.cs
+++ Lab03/obj/Debug/net8.0/Lab03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d901331c83617ae82154e043325c92afb98b14c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
