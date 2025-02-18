87413d920b47b70eb8c6493ac48b2e463f91a7ff
Georges FARDISSI <fardissi.georges@gmail.com>
2025-02-18 11:58:33 +0200

    Finalizing SQL queries

--- Lab03/obj/Debug/net8.0/Lab03.AssemblyInfo.cs
+++ Lab03/obj/Debug/net8.0/Lab03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d901331c83617ae82154e043325c92afb98b14c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e93abd08aff89049a0695ec7d6ff708a9d161bb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
